f2880163199fdf3fce08852a89ce93012641f131
robertosalazare <jrse95@gmail.com>
2017-11-13 22:21:47 -0700

    Agregadas varias regex para la limpieza de datos

--- leer_datos.py
+++ leer_datos.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import re
 
 if __name__ == "__main__":
+    """
+    https://github.com/yannick-c/expander link de un expander de contracciones en inglés,
+    un poco lento pero funciona.
+    """
     """textos = []
     for i in range(4292):
         n = str(i)
@@ -9,17 +13,28 @@ if __name__ == "__main__":
         cadena = f.read()
         soup = BeautifulSoup(cadena, 'html.parser')
         textos.append(soup.get_text())"""
-    eliminar = ['.', ';', ':', '!', '?', '/', '\\', ',', ')', '(', "\"","-",'*','_',"|"]
-    f = open("datos/TESTING_NEW/TEST_00005.eml","r")
+    
+    #se lee el texto del archivo y se vuelve a minúsculas.
+    f = open("datos/TESTING_NEW/TEST_00010.eml","r")
     texto = f.read()
-    texto = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',texto)
-    texto = BeautifulSoup(texto,'html.parser').get_text()
-    #texto = ' '.join(texto.split())
-    #texto = texto.lower()
-    #texto = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',texto)
+    texto = texto.lower()
+
+    #se hace limpieza con regex.
+    texto = re.sub(r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))""",'',texto)
     texto = re.sub(r'[\w\.-]+@[\w\.-]+', " ", texto)
     texto = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', ' ',  texto)
-    #for e in eliminar:
-    #    texto = texto.replace(e,'')
-    #texto = ' '.join(texto.split())
+    texto = re.sub(r'content-type: [\w;/\" -=]*\n','',texto)
+    texto = re.sub(r'content-disposition: [\w;/\" -=]*\n','',texto)
+    texto = re.sub(r'content-transfer-encoding: [\w;/\" -=]*\n','',texto)
+
+    #se eliminan las etiquetas de marcaje.
+    texto = BeautifulSoup(texto,"html.parser").get_text()
+
+    #se eliminan signos de puntuación.
+    eliminar = ['.', ';', ':', '!', '?', '/', '\\', ',', ')', '(', "\"","-",'*','_',"|","[","]"]
+    for e in eliminar:
+        texto = texto.replace(e,'')
+    
+    palabras = texto.split()
+    texto = ' '.join(palabras)
     print(texto)
